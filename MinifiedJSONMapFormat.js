56a49e56dd958b5dd37e3f966de439d24399309f
Rob Loach <robloach@gmail.com>
2023-10-12 05:30:49 -0400

    Initial commit

---
+++ MinifiedJSONMapFormat.js
@@ -0,0 +1,66 @@
+/**********************************************************************************************
+*
+*   Tiled Plugin: Minified JSON Map Format
+*
+*       https://github.com/robloach/tiled-plugins
+*
+*   DESCRIPTION:
+*       This Tiled plugin will export minified JSON files to decrease the size of the
+*       output .json map files.
+*
+*   INSTALL:
+*       1. Add this file to your Tiled extensions directory
+*       2. When exporting, select "Minified JSON map files (*.json)" as the file type
+*
+*   DEPENDENCIES:
+*       Tiled https://www.mapeditor.org/
+*
+*   Copyright 2023 Rob Loach (@RobLoach)
+*
+*   LICENSE: zlib/libpng
+*
+*   This is licensed under an unmodified zlib/libpng license, which is an OSI-certified,
+*   BSD-like license that allows static linking with closed source software:
+*
+*   This software is provided "as-is", without any express or implied warranty. In no event
+*   will the authors be held liable for any damages arising from the use of this software.
+*
+*   Permission is granted to anyone to use this software for any purpose, including commercial
+*   applications, and to alter it and redistribute it freely, subject to the following restrictions:
+*
+*     1. The origin of this software must not be misrepresented; you must not claim that you
+*     wrote the original software. If you use this software in a product, an acknowledgment
+*     in the product documentation would be appreciated but is not required.
+*
+*     2. Altered source versions must be plainly marked as such, and must not be misrepresented
+*     as being the original software.
+*
+*     3. This notice may not be removed or altered from any source distribution.
+*
+**********************************************************************************************/
+var MinifiedJsonMapFormat = {
+    name: "Minified JSON map files",
+    extension: "json",
+
+    write: function(map, fileName) {
+        // Load the original JSON map format.
+        var jsFormat = tiled.mapFormat("json")
+
+        // Write the JSON file using the original map format.
+        jsFormat.write(map, fileName)
+
+        // Read the output from the original JSON map format.
+        var textFile = new TextFile(fileName, TextFile.ReadOnly)
+        var txt = textFile.readAll()
+        textFile.close()
+        var jsonData = JSON.parse(txt)
+
+        // Write the file again, but use JSON.stringify to minify it.
+        textFile = new TextFile(fileName, TextFile.WriteOnly)
+        textFile.truncate()
+        textFile.write(JSON.stringify(jsonData))
+        textFile.commit()
+    }
+}
+
+tiled.registerMapFormat("json", MinifiedJsonMapFormat)
---
+++ README.md
@@ -0,0 +1,17 @@
+# Tiled Extensions
+
+A collection of [Tiled](https://www.mapeditor.org/) extensions.
+
+## Extensions
+
+- [MinifiedJSONMapFormat](MinifiedJSONMapFormat.js) - Export minified JSON for your maps.
+
+## License
+
+Unless stated otherwise, all works are:
+
+- Copyright (c) 2023 [Rob Loach](https://robloach.net)
+
+... and licensed under:
+
+- [zlib License](LICENSE)
